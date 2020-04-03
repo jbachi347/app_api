7f7f8f47b3c538ba35572ab5d00cf60070f7f689
Joel <info@jbachi.com>
2020-04-02 23:04:18 -0500

    Rutas Artisan en Web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,60 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// MIGRATE
+
+Route::get('migrate',  array('as' => 'install', function(){
+
+    try {
+
+      echo '<br>init with app tables migrations...';
+      Artisan::call('migrate', array('--force' => true));
+      echo '<br>done with app tables migrations';
+      
+
+    } catch (Exception $e) {
+      Response::make($e->getMessage(), 500);
+    }
+
+}));
+
+
+//Clear Cache facade value:
+Route::get('/clear-cache', function() {
+  $exitCode = Artisan::call('cache:clear');
+  return '<h1>Cache facade value cleared</h1>';
+});
+
+//Reoptimized class loader:
+Route::get('/optimize', function() {
+  $exitCode = Artisan::call('optimize');
+  return '<h1>Reoptimized class loader</h1>';
+});
+
+//Route cache:
+Route::get('/route-cache', function() {
+  $exitCode = Artisan::call('route:cache');
+  return '<h1>Routes cached</h1>';
+});
+
+//Clear Route cache:
+Route::get('/route-clear', function() {
+  $exitCode = Artisan::call('route:clear');
+  return '<h1>Route cache cleared</h1>';
+});
+
+//Clear View cache:
+Route::get('/view-clear', function() {
+  $exitCode = Artisan::call('view:clear');
+  return '<h1>View cache cleared</h1>';
+});
+
+//Clear Config cache:
+Route::get('/config-cache', function() {
+  $exitCode = Artisan::call('config:cache');
+  return '<h1>Clear Config cleared</h1>';
+});
+
+
